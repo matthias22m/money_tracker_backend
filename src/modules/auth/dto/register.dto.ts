605b4736523184cd9e1fd73cead8100df43c33ad
Matthias <matthias22m@gmail.com>
2025-10-08 07:28:43 -0700

    feat: add DTOs for user login and registration

---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
