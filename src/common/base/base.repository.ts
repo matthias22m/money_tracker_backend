4818d2f7322d15666ca01bac6b5195a5fc0c6f7f
Matthias <matthias22m@gmail.com>
2025-10-08 07:23:29 -0700

    config: base repository

---
+++ src/common/base/base.repository.ts
@@ -0,0 +1,28 @@
+import { Repository, FindOptionsWhere, DeepPartial } from 'typeorm';
+
+export abstract class BaseRepository<T> {
+  constructor(protected readonly repository: Repository<T>) {}
+
+  async findAll(): Promise<T[]> {
+    return this.repository.find();
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return this.repository.findOne({ where: { id } as any });
+  }
+
+  async create(data: DeepPartial<T>): Promise<T> {
+    const entity = this.repository.create(data);
+    return this.repository.save(entity);
+  }
+
+  async update(id: string, data: DeepPartial<T>): Promise<T | null> {
+    await this.repository.update(id, data as any);
+    return this.findById(id);
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const result = await this.repository.delete(id);
+    return result.affected > 0;
+  }
+}
