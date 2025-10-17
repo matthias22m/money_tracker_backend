63b08ede2b15ebc8aff3dad9add680179afb8db8
Matthias <matthias22m@gmail.com>
2025-10-17 00:04:07 -0700

    feat(app): wire groups module into application

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { NotificationModule } from './modules/notification/notification.module';
 import { SettlementModule } from './modules/settlement/settlement.module';
 import { AnalyticsModule } from './modules/analytics/analytics.module';
 import { MailModule } from './modules/mail/mail.module';
+import { GroupsModule } from './modules/groups/groups.module';
 import { AppController } from './app.controller';
 
 @Module({
@@ -27,6 +28,7 @@ import { AppController } from './app.controller';
     SettlementModule,
     AnalyticsModule,
     MailModule,
+    GroupsModule,
   ],
   controllers:[AppController]
 })
